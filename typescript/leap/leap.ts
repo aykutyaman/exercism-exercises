eb79f761935fb9d8eedce4930b0e8e58a2eff800
Aykut Yaman <yaman.ayk@gmail.com>
2018-10-11 10:12:53 +0300

    non-primitive boxed object type Boolean changed with type boolean

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,7 +1,7 @@
-const evenlyDivisibleBy = (x: number, y: number): Boolean => (x % y === 0)
-const notEvenlyDivisibleBy = (x: number, y: number): Boolean => !evenlyDivisibleBy(x, y)
+const evenlyDivisibleBy = (x: number, y: number): boolean => (x % y === 0)
+const notEvenlyDivisibleBy = (x: number, y: number): boolean => !evenlyDivisibleBy(x, y)
 
-const isLeapYear = (year: number): Boolean => (
+const isLeapYear = (year: number): boolean => (
   evenlyDivisibleBy(year, 4)
   && notEvenlyDivisibleBy(year, 100)
   || evenlyDivisibleBy(year, 400)
